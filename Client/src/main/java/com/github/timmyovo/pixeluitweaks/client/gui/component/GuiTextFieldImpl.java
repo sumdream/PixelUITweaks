cc74eef2abad4ff603e8b8bf207ea6b56822908e
TimmyOVO <timmyovo@qq.com>
2019-07-03 02:53:17 +0800

    修复textfield不失去焦点

--- Client/src/main/java/com/github/timmyovo/pixeluitweaks/client/gui/component/GuiTextFieldImpl.java
+++ Client/src/main/java/com/github/timmyovo/pixeluitweaks/client/gui/component/GuiTextFieldImpl.java
@@ -197,7 +197,9 @@ public class GuiTextFieldImpl extends Gui implements ClientComponent<ComponentTe
 
     @Override
     public boolean mousePressed(Minecraft mc, int mouseX, int mouseY) {
-        return mouseX >= this.x && mouseX < this.x + this.width && mouseY >= this.y && mouseY < this.y + this.height && isEnabled;
+        boolean flag = mouseX >= this.x && mouseX < this.x + this.width && mouseY >= this.y && mouseY < this.y + this.height && isEnabled;
+        setFocused(flag);
+        return flag;
     }
 
     /**
