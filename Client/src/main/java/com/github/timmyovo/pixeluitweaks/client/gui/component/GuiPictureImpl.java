d8819b68354276707a0b658c40943adb3ac337db
TimmyOVO <timmyovo@qq.com>
2019-08-04 02:10:22 +0800

    picture添加hover实现

--- Client/src/main/java/com/github/timmyovo/pixeluitweaks/client/gui/component/GuiPictureImpl.java
+++ Client/src/main/java/com/github/timmyovo/pixeluitweaks/client/gui/component/GuiPictureImpl.java
@@ -7,7 +7,10 @@ import com.github.timmyovo.pixeluitweaks.client.utils.TextureUtils;
 import com.github.timmyovo.pixeluitweaks.common.gui.component.impl.ComponentPicture;
 import lombok.Getter;
 import lombok.Setter;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.ScaledResolution;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
 @Getter
 @Setter
@@ -16,6 +19,10 @@ public class GuiPictureImpl extends Gui implements ClientComponent<ComponentPict
     private ClientRenderMethod clientRenderMethod;
     private boolean visible;
     private boolean hovered;
+    public int width;
+    public int height;
+    public int x;
+    public int y;
     private ClientItemRenderer clientItemRenderer;
 
     public GuiPictureImpl(ComponentPicture componentPicture) {
@@ -26,7 +33,7 @@ public class GuiPictureImpl extends Gui implements ClientComponent<ComponentPict
     @Override
     public void render(int mouseX, int mouseY, float ticks) {
         if (visible) {
-
+            this.hovered = mouseX >= x && mouseY >= this.y && mouseX < this.x + this.width && mouseY < this.y + this.height;
             if (componentPicture.getTextureBinder() != null) {
                 TextureUtils.tryBindTexture(componentPicture.getTextureBinder());
             }
@@ -46,11 +53,40 @@ public class GuiPictureImpl extends Gui implements ClientComponent<ComponentPict
 
     @Override
     public boolean isMouseOver() {
-        return false;
+        return hovered;
     }
 
     @Override
     public void updateComponent(ComponentPicture componentModel) {
+        Minecraft mc = Minecraft.getMinecraft();
+        ScaledResolution scaledResolution = new ScaledResolution(mc);
+        int scaledHeight = scaledResolution.getScaledHeight();
+        int scaledWidth = scaledResolution.getScaledWidth();
+
+        this.width = (int) new ExpressionBuilder(componentModel.getWidth())
+                .variables("w", "h")
+                .build()
+                .setVariable("w", scaledWidth)
+                .setVariable("h", scaledHeight)
+                .evaluate();
+        this.height = (int) new ExpressionBuilder(componentModel.getHeight())
+                .variables("w", "h")
+                .build()
+                .setVariable("w", scaledWidth)
+                .setVariable("h", scaledHeight)
+                .evaluate();
+        this.x = (int) new ExpressionBuilder(componentModel.getXPos())
+                .variables("w", "h")
+                .build()
+                .setVariable("w", scaledWidth)
+                .setVariable("h", scaledHeight)
+                .evaluate();
+        this.y = (int) new ExpressionBuilder(componentModel.getYPos())
+                .variables("w", "h")
+                .build()
+                .setVariable("w", scaledWidth)
+                .setVariable("h", scaledHeight)
+                .evaluate();
         if (componentModel.getRenderMethod() != null) {
             this.clientRenderMethod = ClientRenderMethod.fromRenderMethod(componentModel.getRenderMethod());
         }
