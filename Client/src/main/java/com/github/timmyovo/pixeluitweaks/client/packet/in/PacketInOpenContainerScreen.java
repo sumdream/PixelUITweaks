e6fc97cc538fc362b8dc24ee9b45533edb33af9c
TimmyOVO <timmyovo@qq.com>
2019-08-04 01:29:41 +0800

    ...

--- Client/src/main/java/com/github/timmyovo/pixeluitweaks/client/packet/in/PacketInOpenContainerScreen.java
+++ Client/src/main/java/com/github/timmyovo/pixeluitweaks/client/packet/in/PacketInOpenContainerScreen.java
@@ -1,6 +1,9 @@
 package com.github.timmyovo.pixeluitweaks.client.packet.in;
 
 import com.github.timmyovo.pixeluitweaks.client.packet.IPacketIn;
+import com.github.timmyovo.pixeluitweaks.client.packet.manager.LocalDataManager;
+import com.github.timmyovo.pixeluitweaks.common.gui.ComponentContainer;
+import com.github.timmyovo.pixeluitweaks.common.message.GuiFactory;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.PacketBuffer;
 
@@ -9,10 +12,10 @@ public class PacketInOpenContainerScreen implements IPacketIn {
     public void readPacket(PacketBuffer packetBuffer) {
         Minecraft.getMinecraft().addScheduledTask(() -> {
             //实现已被删除,因为不知道客户端缺少了什么神奇的操作所以用forge的服务端包代替打开
-//            int windowId = packetBuffer.readInt();
-//            String containerString = packetBuffer.readString(Short.MAX_VALUE);
-//            ComponentContainer componentContainer = GuiFactory.fromString(containerString, ComponentContainer.class);
-//            LocalDataManager.getSlotComponentContainers().add(componentContainer);
+            int windowId = packetBuffer.readInt();
+            String containerString = packetBuffer.readString(Short.MAX_VALUE);
+            ComponentContainer componentContainer = GuiFactory.fromString(containerString, ComponentContainer.class);
+            LocalDataManager.getSlotComponentContainers().add(componentContainer);
 ////            EntityPlayerSP player = Minecraft.getMinecraft().player;
 ////            SlotUIScreen slotUIScreen = new SlotUIScreen(player, componentContainer);
 ////            Minecraft.getMinecraft().displayGuiScreen(slotUIScreen);
