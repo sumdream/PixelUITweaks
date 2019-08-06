b4d1feb68cfa0597d1f0378789e1663baf4e92e5
TimmyOVO <timmyovo@qq.com>
2019-08-06 23:56:15 +0800

    magic!

--- Client/src/main/java/com/github/timmyovo/pixeluitweaks/client/packet/in/PacketInRecvTexture.java
+++ Client/src/main/java/com/github/timmyovo/pixeluitweaks/client/packet/in/PacketInRecvTexture.java
@@ -3,6 +3,7 @@ package com.github.timmyovo.pixeluitweaks.client.packet.in;
 import com.github.timmyovo.pixeluitweaks.client.PixelUITweaks;
 import com.github.timmyovo.pixeluitweaks.client.packet.IPacketIn;
 import com.github.timmyovo.pixeluitweaks.client.texture.IDownloadTexture;
+import com.google.common.collect.Maps;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.PacketBuffer;
 
@@ -10,32 +11,48 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.zip.GZIPInputStream;
+import java.util.Map;
 
 public class PacketInRecvTexture implements IPacketIn {
+    private static final Map<String, byte[]> PROCESSING_TEXTURES = Maps.newConcurrentMap();
+
+    private static byte[] combineByteArray(byte[] b1, byte[] b2) {
+        byte[] combined = new byte[b1.length + b2.length];
+
+        System.arraycopy(b1, 0, combined, 0, b1.length);
+        System.arraycopy(b2, 0, combined, b1.length, b2.length);
+        return combined;
+    }
+
     @Override
     public void readPacket(PacketBuffer packetBuffer) {
         Minecraft.getMinecraft().addScheduledTask(() -> {
             String name = packetBuffer.readString(32);
-            byte[] buf = packetBuffer.readByteArray();
-            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(buf);
-            GZIPInputStream gzipInputStream = null;
-            try {
-                gzipInputStream = new GZIPInputStream(byteArrayInputStream);
-            } catch (IOException e) {
-                e.printStackTrace();
+            byte b = packetBuffer.readByte();
+            if (b == 0) {
+                byte[] bytesRecv = packetBuffer.readByteArray();
+                byte[] bytes = PROCESSING_TEXTURES.get(name);
+                if (bytes == null) {
+                    PROCESSING_TEXTURES.put(name, bytesRecv);
+                } else {
+                    PROCESSING_TEXTURES.put(name, combineByteArray(bytes, bytesRecv));
+                }
             }
-            if (gzipInputStream == null) {
-                return;
+            if (b == 1) {
+                byte[] buf = PROCESSING_TEXTURES.get(name);
+                ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(buf);
+                BufferedImage bufferedImage = null;
+                try {
+                    bufferedImage = ImageIO.read(byteArrayInputStream);
+                    byteArrayInputStream.close();
+                    PROCESSING_TEXTURES.remove(name);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                PixelUITweaks.INSTANCE.getDownloadTextureManager().getTextureMap().put(name, new IDownloadTexture(bufferedImage));
             }
-            BufferedImage bufferedImage = null;
-            try {
-                bufferedImage = ImageIO.read(gzipInputStream);
-                byteArrayInputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            PixelUITweaks.INSTANCE.getDownloadTextureManager().getTextureMap().put(name, new IDownloadTexture(bufferedImage));
+
         });
     }
 }
--- Server/src/main/java/com/github/timmyovo/pixeluitweaks/server/packet/out_/PacketOutRecvTexture.java
+++ Server/src/main/java/com/github/timmyovo/pixeluitweaks/server/packet/out_/PacketOutRecvTexture.java
@@ -2,44 +2,83 @@ package com.github.timmyovo.pixeluitweaks.server.packet.out_;
 
 import com.github.timmyovo.pixeluitweaks.common.packet.PacketTypes;
 import com.github.timmyovo.pixeluitweaks.server.packet.IPacketOut;
+import io.netty.buffer.Unpooled;
+import net.minecraft.server.v1_12_R1.EntityPlayer;
 import net.minecraft.server.v1_12_R1.PacketDataSerializer;
+import net.minecraft.server.v1_12_R1.PacketPlayOutCustomPayload;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.zip.GZIPOutputStream;
+import java.util.Arrays;
 
 public class PacketOutRecvTexture implements IPacketOut {
     private String name;
     private BufferedImage image;
+    private byte[] bytes;
 
     public PacketOutRecvTexture(String name, BufferedImage image) {
         this.name = name;
         this.image = image;
-    }
-
-    @Override
-    public void writePacket(PacketDataSerializer packetBuffer) {
         ByteArrayOutputStream byteArrayOutputStream = null;
         try {
             byteArrayOutputStream = new ByteArrayOutputStream();
-            GZIPOutputStream gzipOutputStream = new GZIPOutputStream(byteArrayOutputStream);
             try {
-                ImageIO.write(image, "PNG", gzipOutputStream);
+                ImageIO.write(image, "PNG", byteArrayOutputStream);
             } finally {
-                gzipOutputStream.close();
+                byteArrayOutputStream.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        packetBuffer.a(name);
-        packetBuffer.a(byteArrayOutputStream.toByteArray());
-        try {
-            byteArrayOutputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        bytes = byteArrayOutputStream.toByteArray();
+    }
+
+    public static byte[][] divideArray(byte[] source, int chunksize) {
+        byte[][] ret = new byte[(int) Math.ceil(source.length / (double) chunksize)][chunksize];
+
+        int start = 0;
+
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = Arrays.copyOfRange(source, start, start + chunksize);
+            start += chunksize;
         }
+
+        return ret;
+    }
+
+    @Override
+    public void writePacket(PacketDataSerializer packetBuffer) {
+
+    }
+
+    @Override
+    public void sendPacket(EntityPlayer entityPlayer) {
+
+        if (bytes.length <= 20000) {
+            PacketDataSerializer dataPacket = new PacketDataSerializer(Unpooled.buffer());
+            dataPacket.a(name);
+            dataPacket.writeByte(0);
+            dataPacket.a(bytes);
+            entityPlayer.playerConnection.sendPacket(new PacketPlayOutCustomPayload(getPacketType(), dataPacket));
+            PacketDataSerializer finishPacket = new PacketDataSerializer(Unpooled.buffer());
+            finishPacket.a(name);
+            finishPacket.writeByte(1);
+            entityPlayer.playerConnection.sendPacket(new PacketPlayOutCustomPayload(getPacketType(), finishPacket));
+            return;
+        }
+        for (byte[] bytes1 : divideArray(bytes, bytes.length / 20000)) {
+            PacketDataSerializer dataPacket = new PacketDataSerializer(Unpooled.buffer());
+            dataPacket.a(name);
+            dataPacket.writeByte(0);
+            dataPacket.a(bytes1);
+            entityPlayer.playerConnection.sendPacket(new PacketPlayOutCustomPayload(getPacketType(), dataPacket));
+        }
+        PacketDataSerializer finishPacket = new PacketDataSerializer(Unpooled.buffer());
+        finishPacket.a(name);
+        finishPacket.writeByte(1);
+        entityPlayer.playerConnection.sendPacket(new PacketPlayOutCustomPayload(getPacketType(), finishPacket));
     }
 
     @Override
